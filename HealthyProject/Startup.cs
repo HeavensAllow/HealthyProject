f5557f5499c2681c00ee8f2cc0615bb9ffdfbde6
MartaBoavida <31953691+MartaBoavida@users.noreply.github.com>
2017-10-27 14:45:08 +0000

    Revert "Revert "forum lindo""
    This reverts commit a27d8b881ac629485e5240229bcf7ab97d9d2887.

--- HealthyProject/Startup.cs
+++ HealthyProject/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using HealthyProject.Models;
 
 [assembly: OwinStartupAttribute(typeof(HealthyProject.Startup))]
 namespace HealthyProject
@@ -9,6 +12,27 @@ namespace HealthyProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        // In this method we will create default User roles and Admin user for login    
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            
+            var roleManager = new RoleManager<CustomRole,int>(new CustomRoleStore(context));
+            var UserManager = new UserManager<ApplicationUser,int>(new CustomUserStore(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User     
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin role   
+                var role = new CustomRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
