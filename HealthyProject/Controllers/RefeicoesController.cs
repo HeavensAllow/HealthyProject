762567c7d7a98dcc56a49f0ee13a56b676604e94
HeavensAllow <31953742+HeavensAllow@users.noreply.github.com>
2017-11-09 17:18:04 +0000

    Revert "Merge remote-tracking branch 'origin/Pedro' into Andre"
    This reverts commit 43d0b069513a2cd8e6a8e4f818946a0c7d9c2875, reversing
    changes made to 8d07b7f64c6251e7a7fa944f2e5d282f05d16fb4.

--- HealthyProject/Controllers/RefeicoesController.cs
+++ HealthyProject/Controllers/RefeicoesController.cs
@@ -8,7 +8,6 @@ using System.Web;
 using System.Web.Mvc;
 using HealthyProject.Models;
 using Microsoft.AspNet.Identity;
-using System.Data.SqlClient;
 
 namespace HealthyProject.Controllers
 {
@@ -18,7 +17,6 @@ namespace HealthyProject.Controllers
 
         // GET: Refeicoes
         public ActionResult Index()
-        public ActionResult Index(DateTime? dateInput)
         {
             var refeicoes = db.Refeicoes.Include(r => r.RegistoDiario);
             int userId = Convert.ToInt32(User.Identity.GetUserId());
@@ -30,19 +28,8 @@ namespace HealthyProject.Controllers
             }
             return View(refeicoes.ToList());
         }
-            if (dateInput == null)
-            {
-                var refeicoes = db.Refeicoes.Include(r => r.RegistoDiario).Where(d => d.Data == DateTime.Today);
-                return View(refeicoes);
 
         // GET: Refeicoes/Details/5
-            }
-            else
-            {
-                var refeicoes = db.Refeicoes.Include(r => r.RegistoDiario).Where(d => d.Data == dateInput);
-                return View(refeicoes);
-            }
-        }
         public ActionResult Details(int? id)
         {
             int userId = Convert.ToInt32(User.Identity.GetUserId());
@@ -82,23 +69,6 @@ namespace HealthyProject.Controllers
         // POST: Refeicoes/Create
         // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
         // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
-        // POST: Refeicoes
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Indexx(DateTime? dateInput)
-        {
-            if (dateInput == null)
-            {
-                var refeicoes1 = db.Refeicoes.Include(r => r.RegistoDiario).Where(d => d.Data == DateTime.Today);
-                return View(refeicoes1);
-            }
-            else
-            {
-                var refeicoes = db.Refeicoes.Include(r => r.RegistoDiario).Where(d => d.Data == dateInput);
-                return View(refeicoes);
-            }
-        }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "RefeicaoID,RegistoID,Data,Tipo")] Refeico refeico)
@@ -108,63 +78,6 @@ namespace HealthyProject.Controllers
                 db.Refeicoes.Add(refeico);
                 db.SaveChanges();
                 return RedirectToAction("Index");
-                double counterkcal = 0, counterproteinas = 0, countergordura = 0, counterhc = 0;
-                var registo = db.RegistoDiarios.FirstOrDefault(p => p.Data == DateTime.Today);
-                if (registo == null)
-                {
-                    RegistoDiario registo1 = new RegistoDiario();
-                    var refeicoes = db.RefeicaoPratos.Include(i => i.Refeico).Where(q => q.Refeico.RegistoID == registo1.RegistoID);
-                    var user = Convert.ToInt32(User.Identity.GetUserId());
-                    Objectivo objectivo = db.Objectivoes.FirstOrDefault(o => o.UserID == user && o.Data_fim == null);
-                    if (objectivo != null)
-                    {
-                        registo1.ObjectivoID = objectivo.ObjectivoID;
-                        db.RegistoDiarios.Add(registo1);
-                        foreach(RefeicaoPrato i in refeicoes)
-                        {
-                            counterkcal += (i.Dose / 100) * i.Prato.Kcal;
-                            counterproteinas += (i.Dose / 100) * i.Prato.Proteinas;
-                            countergordura += (i.Dose / 100) * i.Prato.Gordura;
-                            counterhc += (i.Dose / 100) * i.Prato.HidCarbono;
-                        }
-                        refeico.RegistoID = registo1.RegistoID;
-                        db.Refeicoes.Add(refeico);
-                        db.SaveChanges();
-                        return RedirectToAction("Index");
-                    }
-                    else
-                    {
-                        TempData["Alert2"] = "Ainda não existe nenhum objectivo. Por favor, crie um objectivo primeiro.";
-                        return RedirectToAction("Index");
-                    }
-                }
-                else
-                {
-                    var user = Convert.ToInt32(User.Identity.GetUserId());
-                    var refeicoes = db.RefeicaoPratos.Include(i => i.Refeico).Where(q => q.Refeico.RegistoID == registo.RegistoID);
-                    Objectivo objectivo = db.Objectivoes.FirstOrDefault(o => o.UserID == user && o.Data_fim == null);
-                    if (objectivo != null)
-                    {
-                        registo.ObjectivoID = objectivo.ObjectivoID;
-                        foreach (RefeicaoPrato i in refeicoes)
-                        {
-                            counterkcal += (i.Dose / 100) * i.Prato.Kcal;
-                            counterproteinas += (i.Dose / 100) * i.Prato.Proteinas;
-                            countergordura += (i.Dose / 100) * i.Prato.Gordura;
-                            counterhc += (i.Dose / 100) * i.Prato.HidCarbono;
-                        }
-                        db.RegistoDiarios.Add(registo);
-                        refeico.RegistoID = registo.RegistoID;
-                        db.Refeicoes.Add(refeico);
-                        db.SaveChanges();
-                        return RedirectToAction("Index");
-                    }
-                    else
-                    {
-                        TempData["Alert2"] = "Ainda não existe nenhum objectivo. Por favor, crie um objectivo primeiro.";
-                        return RedirectToAction("Index");
-                    }
-                }
             }
 
             ViewBag.RegistoID = new SelectList(db.RegistoDiarios, "RegistoID", "RegistoID", refeico.RegistoID);
@@ -244,21 +157,6 @@ namespace HealthyProject.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult NutriInfo(int RefeicaoID)
-        {
-            SqlParameter refID = new SqlParameter("@RefeicaoID", RefeicaoID);
-            IList<CounterInfoRefeicao_Result> NutriInfo = db.Database.SqlQuery<CounterInfoRefeicao_Result>("dbo.CounterInfoRefeicao @RefeicaoID", refID).ToList<CounterInfoRefeicao_Result>();
-            return View(NutriInfo);  //Andre diz para usar PartialView
-        }
-
-        public ActionResult KcalInfo(int RefeicaoID)
-        {
-            SqlParameter refID = new SqlParameter("@RefeicaoID", RefeicaoID);
-            IList<CounterKcalRefeicao_Result> KcalInfo = db.Database.SqlQuery<CounterKcalRefeicao_Result>("dbo.CounterInfoRefeicao @RefeicaoID", refID).ToList<CounterKcalRefeicao_Result>();
-            ViewBag.KcalInfo = KcalInfo;
-            return View();
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -268,3 +166,4 @@ namespace HealthyProject.Controllers
             base.Dispose(disposing);
         }
     }
+}
--- HealthyProject/Models/Refeico.cs
+++ HealthyProject/Models/Refeico.cs
@@ -20,7 +20,6 @@ namespace HealthyProject.Models
             this.RefeicaoBebidas = new HashSet<RefeicaoBebida>();
             this.RefeicaoIngredientes = new HashSet<RefeicaoIngrediente>();
             this.RefeicaoPratos = new HashSet<RefeicaoPrato>();
-            this.RefeicaoBebidas = new HashSet<RefeicaoBebida>();
         }
     
         public int RefeicaoID { get; set; }
@@ -35,7 +34,5 @@ namespace HealthyProject.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<RefeicaoPrato> RefeicaoPratos { get; set; }
         public virtual RegistoDiario RegistoDiario { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<RefeicaoBebida> RefeicaoBebidas { get; set; }
     }
 }
